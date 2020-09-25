58580502aa766d353c0ad8fd32fd8a908f8f898d
davids201221 <68470768+davids201221@users.noreply.github.com>
2020-09-24 21:51:09 -0500

    Add files via upload

---
+++ Carrito de compras/layout/menu.php
@@ -0,0 +1,13 @@
+<nav>
+        <ul>
+            <li><a href="index.php">Comida</a></li>
+            <li><a href="combo.php">Combo</a></li>
+            <li class="carrito">
+                <a href="#" class='btn-carrito'>Carrito</a>
+                <div id="carrito-container">
+                    <div id="tabla">
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </nav>
